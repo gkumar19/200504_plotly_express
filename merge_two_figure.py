e9d1b405de4f9b601321a4587ecfd308dcd1053e
Gaurav Kumar <gkumar.iitk@gmail.com>
2020-08-05 16:09:36 +0530

    Update merge_two_figure.py

--- merge_two_figure.py
+++ merge_two_figure.py
@@ -1,14 +1,23 @@
 import pandas as pd
 import plotly.express as px
-import pickle
+import plotly.graph_objects as go
 
 fig1 = px.line(df, x='P', y='F', color='V', template='seaborn').update_layout(showlegend=True)
 fig2 = px.line(df_requirement, x='pressure', y='flow', color='temperature')
 
-import plotly.graph_objects as go
 fig = go.Figure()
 fig.add_traces(fig1.data)
 fig.add_traces(fig2.data)
 fig.update_xaxes(title_text='Pressure (KPA)')
 fig.update_yaxes(title_text='Flow (l/h)')
+fig.show()
+
+#one more example with custom legend
+fig1 = px.line(y=g_loss_list).update_traces(showlegend=True, name='g_loss')
+fig2 = px.line(y=d_loss_list).update_traces(showlegend=True, name='d_loss')
+
+fig = go.Figure()
+fig.add_traces(fig1.data)
+fig.add_traces(fig2.data)
+fig.update_layout(showlegend=True)
 fig.show()
